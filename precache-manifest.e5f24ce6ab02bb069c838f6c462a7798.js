e15738b3a00fd1f9945fd9d48fb902d18d4c4880
rajankarmakar <rajankarmakar@hotmail.com>
2019-07-04 14:13:55 +0600

    Updates

---
+++ precache-manifest.e5f24ce6ab02bb069c838f6c462a7798.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cbdbfad45d95db2a0400d599e43882f",
+    "url": "/reactapp/index.html"
+  },
+  {
+    "revision": "6bfd5323347f7c20c543",
+    "url": "/reactapp/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/reactapp/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "6bfd5323347f7c20c543",
+    "url": "/reactapp/static/js/main.b82c2d69.chunk.js"
+  },
+  {
+    "revision": "699d48caefa08891bb24",
+    "url": "/reactapp/static/js/runtime~main.7f5faac6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
